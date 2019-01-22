3cad7e6ec6a7176a14c65a4064008549eab38e61
anjasmadani <anjasmadany@gmail.com>
2019-01-23 01:32:45 +0700

    Update home.php

--- home.php
+++ home.php
@@ -7,38 +7,49 @@
             <!-- Widgets -->
             <div class="row clearfix">
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box bg-pink hover-expand-effect">
+                    <div class="info-box-2 bg-pink hover-zoom-effect">
                         <div class="icon">
                             <i class="material-icons">date_range</i>
                         </div>
                         <div class="content">
                             <div class="text"><b>Kalender</b></div>
-                            <div class="number count-to" data-from="0" data-to="125" data-speed="15" data-fresh-interval="20"></div>
+                            <div></div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box bg-cyan hover-expand-effect">
+                    <div class="info-box-2 bg-cyan hover-zoom-effect">
                         <div class="icon">
-                            <i class="material-icons">local_grocery_store</i>
+                            <i class="material-icons">access_time</i>
                         </div>
                         <div class="content">
-                            <div class="text"><b>Penjualan Hari ini</b></div>
+                            <div class="text"><b>Jam</b></div>
                             <div class="number count-to" data-from="0" data-to="257" data-speed="1000" data-fresh-interval="20"></div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-                    <div class="info-box bg-light-green hover-expand-effect">
+                    <div class="info-box-2 bg-light-green hover-zoom-effect">
+                        <div class="icon">
+                            <i class="material-icons">local_grocery_store</i>
+                        </div>
+                        <div class="content">
+                            <div class="text"><b>Penjualan Hari Ini</b></div>
+                            <div class="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20"></div>
+                        </div>
+                    </div>
+                </div> 
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <div class="info-box-2 bg-orange hover-zoom-effect">
                         <div class="icon">
                             <i class="material-icons">trending_up</i>
                         </div>
                         <div class="content">
                             <div class="text"><b>Profit Hari Ini</b></div>
-                            <div class="number count-to" data-from="0" data-to="243" data-speed="1000" data-fresh-interval="20"></div>
+                            <div class="number count-to" data-from="0" data-to="1225" data-speed="1000" data-fresh-interval="20"></div>
                         </div>
                     </div>
-                </div>               
+                </div>              
             </div>
  <!-- Diagram -->
   <!-- Morris Css -->
